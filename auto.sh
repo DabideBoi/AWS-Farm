1954ff20bfa3c6b36b82ab377fc35b7eb05bc018
David Yabis <davidyabis23@gmail.com>
2023-04-11 06:52:44 +0000

    auto commit - 1681195964

--- auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-project_dir="~/AWS-Farm"
-ts=$(date +%s)
-echo "$ts" > $project_dir/main.txt
-git -C $project_dir pull
-git -C $project_dir add .
-git -C $project_dir commit -m "auto commit - $ts"
-git -C $project_dir push origin main --force
